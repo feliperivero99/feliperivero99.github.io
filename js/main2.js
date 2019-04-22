67e750cec3ed43c21afd105035194aeb27b7adbc
feliperivero99 <felipe.riverot@gmail.com>
2019-04-22 18:43:19 -0400

    proceso

--- js/main2.js
+++ js/main2.js
@@ -6,6 +6,15 @@ function myspanish() {
 	Cookies.set("idiom", "es");
 	document.getElementById( 'flageng' ).style.display = 'block';
 	document.getElementById( 'flagspa' ).style.display = 'none';
+	$('.curriculumesp').css('display','block');
+	$('.curriculumeng').css('display','none');
+	$("#histoli2").text("Inicio");
+	$("#histoli3").text("paso 1");
+	$("#histoli4").text("paso 2");
+	$("#histoli5").text("paso 3");
+	$("#introhead").text("Metodología de Trabajo");
+	$("#introlead").text("Desde diseño hasta programación, una vision detallada de como manejo el desarrollo de un proyecto");
+	$("#tellmore").text("Dime Mas");
 	
 }
 
@@ -13,6 +22,15 @@ function myenglish() {
     Cookies.set("idiom", "en");
 	document.getElementById( 'flageng' ).style.display = 'none';
 	document.getElementById( 'flagspa' ).style.display = 'block';
+	$('.curriculumesp').css('display','none');
+	$('.curriculumeng').css('display','block');
+	$("#histoli2").text("Home");
+	$("#histoli3").text("Step 1");
+	$("#histoli4").text("Step 2");
+	$("#histoli5").text("paso 3");
+	$("#introhead").text("Work process");
+	$("#introlead").text("From wireframe to code, here’s a detailed look at how I run a normal project");
+	$("#tellmore").text("Tell me More");
 
 }
 
